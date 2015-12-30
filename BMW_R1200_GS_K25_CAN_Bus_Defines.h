fa2dc6b6c90c76bcb85b09761d6d720d17aa6c1b
Gregg Golembeski <gregg@getdor.com>
2015-12-29 22:30:55 -0500

    Move the MSG IDs from https://docs.google.com/spreadsheets/u/2/d/1tUrOES5fQZa92Robr6uP8v2dzQDq9ohHjUiTU3isqdc/edit#gid=0 to this header file

---
+++ BMW_R1200_GS_K25_CAN_Bus_Defines.h
@@ -0,0 +1,14 @@
+
+// MSG IDs for modules on a BMW R1200 GS, K25
+#define MSG_ID_BMSK_Control_Module     0x10C // Throttle position, engine rpm, clutch lever, kill switch & side stand switch
+#define MSG_ID_BMSK Control Module_2   0x2BC // Gears, Engine Temperature & Air Temperature
+
+#define MSG_ID_ZFE_Control_Module      0x130 // Lighting: turn signals & high beam
+#define MSG_ID_ZFE Control Module_2    0x2D0 // LAMPF, Fuel Level & Control buttons: info, heated grip & esa
+
+#define MSG_ID_ABS_Control_Module      0x294 // Speed and distance traveled on front wheel, ABS status & brake levers
+#define MSG_ID_ABS Control Module_2    0x2A8 // Speed and distance traveled on rear wheel
+
+#define MSG_ID_Instrument_Cluster      0x3F8 // Odometer in kilometers
+#define MSG_ID_Instrument_Cluster_2    0x3FF // Ambient Light, Clock
+
