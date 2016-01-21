2a936a325d965d998860a582aae86ec6fbe85e70
Gregg Golembeski <gregg@getdor.com>
2016-01-21 15:07:56 -0800

    Fix global namespace issues with enums

--- Event_Printer/BMW_R1200_GS_K25_State.h
+++ Event_Printer/BMW_R1200_GS_K25_State.h
@@ -1,54 +1,66 @@
 
-#define THROTTLE_PERCENTAGE(x) ( x / 255 ) * 100
+#define K25_THROTTLE_PERCENTAGE(x) ( x / 255 ) * 100
 
-enum Heated_Grips_State
+enum K25_Heated_Grips_State
 {
-    off  = 0x0F,
-    low  = 0x0C,
-    high = 0x0D
-}
+    K25_Heated_Grips_State_off  = 0x0F,
+    K25_Heated_Grips_State_low  = 0x0C,
+    K25_Heated_Grips_State_high = 0x0D
+};
 
-enum Turn_Signals_State
+enum K25_Turn_Signals_State
 {
-    off   = 0xCF,
-    left  = 0xD7,
-    right = 0xE7,
-    both  = 0xEF
-}
+    K25_Turn_Signals_State_off   = 0xCF,
+    K25_Turn_Signals_State_left  = 0xD7,
+    K25_Turn_Signals_State_right = 0xE7,
+    K25_Turn_Signals_State_both  = 0xEF
+};
 
-enum High_Beam_State
+enum K25_High_Beam_State
 {
-    off = 0x0A,
-    on  = 0x09
-}
+    K25_High_Beam_State_off = 0x0A,
+    K25_High_Beam_State_on  = 0x09
+};
 
-enum Info_Button_State
+enum K25_Info_Button_State
 {
-    short_press = 0x05,
-    long_press = 0x06
-}
+    K25_Info_Button_State_short_press = 0x05,
+    K25_Info_Button_State_long_press = 0x06
+};
 
-enum Clutch_Lever_State
+enum K25_Clutch_Lever_State
 {
-    out = 0x06,
-    in  = 0x0A
-}
+    K25_Clutch_Lever_State_out = 0x06,
+    K25_Clutch_Lever_State_in  = 0x0A
+};
 
-enum Brake_Lever_State
+enum K25_Brake_Lever_State
 {
-    none  = 0x03,
-    front = 0x07,
-    rear  = 0x0B
-}
+    K25_Brake_Lever_State_none  = 0x03,
+    K25_Brake_Lever_State_front = 0x07,
+    K25_Brake_Lever_State_rear  = 0x0B
+};
 
-enum ABS_State
+enum K25_ABS_State
 {
-    off = 0x0B,
-    on  = 0x05
-}
+    K25_ABS_State_off = 0x0B,
+    K25_ABS_State_on  = 0x05
+};
 
-enum Ambient_Light_Sensor_State
+enum K25_ALS_State
 {
-    dark  = 0x07,
-    light = 0x03
-}
+    K25_ALS_State_dark  = 0x07,
+    K25_ALS_State_light = 0x03
+};
+
+struct K25_State
+{
+    K25_Heated_Grips_State heated_grips;
+    K25_Turn_Signals_State turn_signals;
+    K25_High_Beam_State high_beam;
+    K25_Info_Button_State info_button;
+    K25_Clutch_Lever_State clutch;
+    K25_Brake_Lever_State brake_levers;
+    K25_ABS_State abs_system;
+    K25_ALS_State als;
+};
