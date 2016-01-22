a625b6c34aea2a2bb1f16f31486403ed9da05754
Gregg Golembeski <gregg@getdor.com>
2016-01-21 16:26:29 -0800

    Greatly reduce file sizes by processing in the switch statement. Right now it doens't need to be any more complicated

--- Event_Printer/Event_Printer.ino
+++ Event_Printer/Event_Printer.ino
@@ -50,52 +50,52 @@ void print_motorcycle_state()
     if( motorcycle_state.heated_grips == K25_Heated_Grips_State_off ) Serial.print("OFF");
     else if( motorcycle_state.heated_grips == K25_Heated_Grips_State_low ) Serial.print("LOW");
     else if( motorcycle_state.heated_grips == K25_Heated_Grips_State_high ) Serial.print("HIGH");
-    else Serial.print("Unknown"); 
+    else Serial.print("Unknown");
     Serial.println();
 
     Serial.print("Turn Signals: ");
     if( motorcycle_state.turn_signals == K25_Turn_Signals_State_off ) Serial.print("OFF");
     else if( motorcycle_state.turn_signals == K25_Turn_Signals_State_left ) Serial.print("LEFT");
     else if( motorcycle_state.turn_signals == K25_Turn_Signals_State_right ) Serial.print("RIGHT");
-    else if( motorcycle_state.turn_signals == K25_Turn_Signals_State_both ) Serial.print("HAZARDS");    
-    else Serial.print("Unknown"); 
+    else if( motorcycle_state.turn_signals == K25_Turn_Signals_State_both ) Serial.print("HAZARDS");
+    else Serial.print("Unknown");
     Serial.println();
 
     Serial.print("High Beam: ");
     if( motorcycle_state.high_beam == K25_High_Beam_State_off ) Serial.print("OFF");
-    else if( motorcycle_state.high_beam == K25_High_Beam_State_on ) Serial.print("ON");   
-    else Serial.print("Unknown"); 
+    else if( motorcycle_state.high_beam == K25_High_Beam_State_on ) Serial.print("ON");
+    else Serial.print("Unknown");
     Serial.println();
 
     Serial.print("Info Button: ");
     if( motorcycle_state.info_button == K25_Info_Button_State_short_press ) Serial.print("SHORT");
-    else if( motorcycle_state.info_button == K25_Info_Button_State_long_press ) Serial.print("LONG");   
-    else Serial.print("Unknown"); 
+    else if( motorcycle_state.info_button == K25_Info_Button_State_long_press ) Serial.print("LONG");
+    else Serial.print("Unknown");
     Serial.println();
 
     Serial.print("Clutch: ");
     if( motorcycle_state.clutch == K25_Clutch_Lever_State_out ) Serial.print("OUT");
-    else if( motorcycle_state.clutch == K25_Clutch_Lever_State_in ) Serial.print("IN");   
-    else Serial.print("Unknown"); 
+    else if( motorcycle_state.clutch == K25_Clutch_Lever_State_in ) Serial.print("IN");
+    else Serial.print("Unknown");
     Serial.println();
 
     Serial.print("Brakes: ");
     if( motorcycle_state.brake_levers == K25_Brake_Lever_State_none ) Serial.print("NONE");
-    else if( motorcycle_state.brake_levers == K25_Brake_Lever_State_front ) Serial.print("FRONT");  
-    else if( motorcycle_state.brake_levers == K25_Brake_Lever_State_rear ) Serial.print("REAR");  
-    else Serial.print("Unknown"); 
+    else if( motorcycle_state.brake_levers == K25_Brake_Lever_State_front ) Serial.print("FRONT");
+    else if( motorcycle_state.brake_levers == K25_Brake_Lever_State_rear ) Serial.print("REAR");
+    else Serial.print("Unknown");
     Serial.println();
 
     Serial.print("ABS: ");
     if( motorcycle_state.abs_system == K25_ABS_State_off ) Serial.print("OFF");
-    else if( motorcycle_state.abs_system == K25_ABS_State_on ) Serial.print("ON");  
-    else Serial.print("Unknown"); 
+    else if( motorcycle_state.abs_system == K25_ABS_State_on ) Serial.print("ON");
+    else Serial.print("Unknown");
     Serial.println();
 
     Serial.print("ALS: ");
     if( motorcycle_state.als == K25_ALS_State_dark ) Serial.print("DARK");
-    else if( motorcycle_state.als == K25_ALS_State_light ) Serial.print("LIGHT");  
-    else Serial.print("Unknown"); 
+    else if( motorcycle_state.als == K25_ALS_State_light ) Serial.print("LIGHT");
+    else Serial.print("Unknown");
     Serial.println();
 }
 
@@ -138,7 +138,8 @@ void process_CAN_Messages()
         {
           case MSG_ID_BMSK_Control_Module:
           {
-            handle_MSG_ID_BMSK_Control_Module(data, length);
+            motorcycle_state.throttle_position = (K25_Throttle_Position_t)K25_THROTTLE_PERCENTAGE(data[1]);
+            motorcycle_state.clutch = (K25_Clutch_Lever_State)LO_NIBBLE(data[4]);
           } break;
 
           case MSG_ID_BMSK_Control_Module_2:
@@ -148,17 +149,20 @@ void process_CAN_Messages()
 
           case MSG_ID_ZFE_Control_Module:
           {
-            handle_MSG_ID_ZFE_Control_Module(data, length);
+            motorcycle_state.high_beam = (K25_High_Beam_State)LO_NIBBLE(data[6]);
+            motorcycle_state.turn_signals = (K25_Turn_Signals_State)data[7];
           } break;
 
           case MSG_ID_ZFE_Control_Module_2:
           {
-            handle_MSG_ID_ZFE_Control_Module_2(data, length);
+              motorcycle_state.heated_grips = (K25_Heated_Grips_State)HI_NIBBLE(data[7]);
+              motorcycle_state.info_button = (K25_Info_Button_State)HI_NIBBLE(data[5]);
           } break;
 
           case MSG_ID_ABS_Control_Module:
           {
-            handle_MSG_ID_ABS_Control_Module(data, length);
+            motorcycle_state.brake_levers = (K25_Brake_Lever_State)LO_NIBBLE(data[6]);
+            motorcycle_state.abs_system = (K25_ABS_State)HI_NIBBLE(data[1]);
           } break;
 
           case MSG_ID_ABS_Control_Module_2:
@@ -168,70 +172,13 @@ void process_CAN_Messages()
 
           case MSG_ID_Instrument_Cluster:
           {
-            handle_MSG_ID_Instrument_Cluster(data, length);
+
           } break;
 
           case MSG_ID_Instrument_Cluster_2:
           {
-            handle_MSG_ID_Instrument_Cluster_2(data, length);
+            motorcycle_state.als = (K25_ALS_State)LO_NIBBLE(data[1]);
           } break;
         }
      }
 }
-
-void handle_MSG_ID_Instrument_Cluster(byte *data, byte length)
-{
-    DEBUG_PRINT("Instrument Cluster: ");
-}
-
-void handle_MSG_ID_Instrument_Cluster_2(byte *data, byte length)
-{
-    // AMBIENT LIGHT SENSOR
-    byte value = LO_NIBBLE(data[1]);
-    motorcycle_state.als = (K25_ALS_State)value;
-}
-
-void handle_MSG_ID_ABS_Control_Module(byte *data, byte length)
-{
-    // BRAKE LEVERS
-    byte value = LO_NIBBLE(data[6]);
-    motorcycle_state.brake_levers = (K25_Brake_Lever_State)value;
-
-    // ABS Status
-    value = HI_NIBBLE(data[1]);
-    motorcycle_state.abs_system = (K25_ABS_State)value;
-}
-
-void handle_MSG_ID_BMSK_Control_Module(byte *data, byte length)
-{
-    // THROTTLE POSITION
-    float fvalue = data[1];
-    motorcycle_state.throttle_position = (K25_Throttle_Position_t)K25_THROTTLE_PERCENTAGE(fvalue);
-
-    // CLUTCH LEVER
-    byte value = LO_NIBBLE(data[4]);
-    motorcycle_state.clutch = (K25_Clutch_Lever_State)value;
-}
-
-void handle_MSG_ID_ZFE_Control_Module(byte *data, byte length)
-{
-    // HIGH BEAM
-    byte value = LO_NIBBLE(data[6]);
-    motorcycle_state.high_beam = (K25_High_Beam_State)value;
-
-    // TURN SIGNAL
-    value = data[7];
-    motorcycle_state.turn_signals = (K25_Turn_Signals_State)value;    
-}
-
-void handle_MSG_ID_ZFE_Control_Module_2(byte *data, byte length)
-{
-    // HEATED GRIPS
-    byte value = HI_NIBBLE(data[7]);
-    motorcycle_state.heated_grips = (K25_Heated_Grips_State)value;    
-
-    // INFO BUTTON
-    value = HI_NIBBLE(data[5]);
-    motorcycle_state.info_button = (K25_Info_Button_State)value;
-}
-
